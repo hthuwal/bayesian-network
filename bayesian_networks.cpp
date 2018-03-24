9ccc3d5cc89c88215880da20ee6628dc99338cb6
Harish Thuwal <hthuwal@gmail.com>
2018-03-24 17:45:42 +0530

    You were over confident and over-estimated yourself and couldn't do it on time and so doing it the next day -_-

--- bayesian_networks.cpp
+++ bayesian_networks.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define vvs vector<vector<string> >
-
+#define vf vector<float>
 class Node{
 
 private:
@@ -26,7 +26,22 @@ public:
     {
         return values_n[v];
     }
+    void init_cpt(float c){
+         float column_size = 1.0;
+        for(int i=0;i< parents.size(); i++)
+            column_size *= parents[i]->get_num_values();
 
+        for (int i=0; i<column_size; i++)
+        {
+            int j=i;
+            while(j<cpt.size())
+            {
+
+                cpt[j] = c/(c*num_values);
+                j += column_size;
+            }
+        }
+    }
     string get_name(){
         return this->name;
     }
@@ -35,7 +50,13 @@ public:
         return this->index;
     }
 
+    float get_cpt_value(int i){
+        return this->cpt[i];
+    }
 
+    vector<string>& get_values(){
+        return this->values;
+    }
     int get_num_values(){
         return this->num_values;
     }
@@ -47,6 +68,11 @@ public:
     vector<Node *>& get_Parents()
     {
         return parents;
+    } 
+
+    vector<Node *>& get_childs()
+    {
+        return childrens;
     }
 
     void add_child(Node* child){
@@ -71,7 +97,11 @@ public:
             cout<<cpt[i]<<" ";
         cout<<endl;;
     }
-	// TODO: add functions as needed
+
+    vf& getcpt()
+    {
+        return cpt;
+    }
 };
 
 class Network{
@@ -91,42 +121,79 @@ public:
 
         cout<< "Can't find the node";
     }
-
+    Node* get_node(int i)
+    {
+        return &nodes[i];
+    }
     void print(){
         for(int i=0; i<nodes.size(); i++)
+        {
             nodes[i].print();
+            nodes[i].print_cpt();
+        }
+    }
+    vector<Node>& get_nodes()
+    {
+        return this->nodes;
     }
 };
 
 class Data{
-
 private:
     vvs data;
+    vf weights;
+    vector<int> u_x;
+    vector<int> u_y;
 public:
+    Data(vvs data, vf weights){
+        this->data = data;
+        this->weights = weights;
+    }
     Data(string file)
     {
         cout<<"Reading Data\n";
         ifstream infile(file);
         string line;
         if(infile.is_open())
-        {
+        {   
+            int i=0;
             while(getline(infile, line))
             {
                   stringstream ss;
                   ss.str(line);
                   vector<string> temp;
                   string value;
+                  int j = 0;
                   while(ss >> value)
+                  {
+                    if(value == "\"?\"")
+                    {
+                        u_x.push_back(i);
+                        u_y.push_back(j);
+                    }
                     temp.push_back(value);
-
+                    j+=1;
+                  }
                   (this->data).push_back(temp);
-
+                  (this->weights).push_back(1);
+                  i++;
             }
             infile.close();
         }
     }
     vvs& get_data(){
         return this->data;
+    }  
+    vf& get_weights(){
+        return this->weights;
+    }
+    vector<int>& get_ux()
+    {
+        return this->u_x;
+    }
+    vector<int>& get_uy()
+    {
+        return this->u_y;
     }
     void print()
     {
@@ -137,16 +204,15 @@ public:
             cout<<endl;
         }
     }
-
 };
 
 
-Network read_network()
+Network read_network(char *file)
 {
 	Network Alarm;
 	string line;
 	int find=0;
-  	ifstream myfile("alarm.bif"); 
+  	ifstream myfile(file); 
   	string temp;
   	string name;
     vector<string> values;
@@ -232,10 +298,159 @@ Network read_network()
   	return Alarm;
 }
 
-// void calc_prob_for_missing()
-// {
-//     return 0.5;
-// }
+void write_network(Network& net)
+{
+    Network Alarm;
+    string line;
+    int find=0;
+    ifstream myfile("alarm.bif"); 
+    ofstream outfile("solved_alarm.bif");
+    string temp;
+    string name;
+
+    if(myfile.is_open())
+    {
+        cout<<"Wrting Network\n";
+        while(!myfile.eof())
+        {
+            stringstream ss;
+            getline (myfile,line);
+            ss.str(line);
+            ss>>temp;
+           
+            if(temp.compare("probability")==0)
+            {
+                    ss>>name;
+                    ss>>name;
+                    outfile << line <<endl;
+                    getline (myfile,line);
+                    Node* node = net.search_node(name);
+                    outfile<<"\ttable ";
+                    vf &cpt = node->getcpt();
+                    for(int i=0;i<cpt.size(); i++)
+                        outfile<<setprecision(2)<<std::fixed<<cpt[i]<<" ";
+                    outfile<<";\n";
+            }       
+            else if(line == "")
+                outfile<<line;
+            else
+                outfile<<line<<endl;
+        }
+        myfile.close();
+    }   
+}
+
+float get_prob_given_par(int line, Node& node, int value, Data& data)
+{
+    vvs& d = data.get_data();
+    vector<int> parents_index;
+    vector<int> parents_num_values;
+    vector<Node*>& parents = node.get_Parents();
+    for(int i=0; i<parents.size(); i++)
+    {
+        parents_index.push_back(parents[i]->get_index());
+        parents_num_values.push_back(parents[i]->get_num_values());
+    }
+
+    int index = 0;
+    int old = 1;
+    for(int j=parents_num_values.size()-1; j>=0; j--)
+    {
+        string value_of_parj = d[line][parents_index[j]];
+        index += parents[j]->get_value_index(value_of_parj)*old;
+        old *= parents_num_values[j];
+    }
+    index += value*old;
+
+    return node.get_cpt_value(index);
+}
+
+string probs(int line, Node& node, Data& data)
+{
+    vvs &d = data.get_data();
+    vector<Node*> &childs = node.get_childs();
+    vector<string>& vals = node.get_values();
+    
+    vector<float> weights;
+    float sum = 0.0;
+    for(int i=0; i<vals.size(); i++)
+    {
+        float prob = get_prob_given_par(line, node, i, data);
+        d[line][node.get_index()] = vals[i];
+
+        for(int j=0; j<childs.size(); j++)
+        {
+            Node child = *childs[j];
+            string v = d[line][child.get_index()];
+            prob = prob * get_prob_given_par(line, child, child.get_value_index(v), data);
+        }
+        weights.push_back(prob);
+        sum += prob;
+    }
+    int max_i = -1;
+    float max_prob = -1;
+    for(int i=0; i<weights.size(); i++)
+    {
+        if(weights[i] > max_prob)
+        {
+            max_i = i;
+            max_prob = weights[i];
+        }
+    }
+    return vals[max_i];
+}
+
+void get_new_data3(Data& data, Network &net)
+{
+    vvs& d = data.get_data();
+    vector<int>& u_x = data.get_ux();
+    vector<int>& u_y = data.get_uy();
+    for(int i = 0 ;i<u_x.size();i++)
+    {
+        int x_u = u_x[i];
+        int y_u = u_y[i];
+
+        Node *node = net.get_node(y_u);
+        vector<string>& vals = node->get_values();
+
+        int max_i = -1;
+        float max_prob = -1;     
+        d[x_u][y_u] = probs(i, *node, data);
+    }
+}
+
+
+
+vf calculate_probs(int i, Node& node, Data& data, int loc_mark, Network& net)
+{
+    vvs& d = data.get_data();
+    vector<float> weights(0);
+    float probs_sum = 0.0;
+    
+    vector<string> &values = node.get_values();
+    
+    for(int j=0; j< values.size();j++)
+    {
+        float prob = get_prob_given_par(i, node, j, data);
+        
+        vector<Node*> &child = node.get_childs();
+        d[i][node.get_index()] = values[j];
+        
+        for(int k = 0; k<child.size(); k++)
+        {
+            string x = d[i][child[k]->get_index()];
+
+            prob *= get_prob_given_par(i,*child[k], child[k]->get_value_index(x), data);
+        }
+        probs_sum += prob;
+        weights.push_back(prob);
+    }
+    for(int j=0 ;j<weights.size(); j++)
+        weights[j] = weights[j]/probs_sum;
+
+    return weights;
+}
+
 void update_cpt(Node& node, Data& data, float c)
 {
     vvs& d = data.get_data();
@@ -255,42 +470,84 @@ void update_cpt(Node& node, Data& data, float c)
 
     vector<float> new_cpt(cpt_size, 0.0);
     
+    float column_size = 1.0;
+    for(int i=0;i< parents_num_values.size(); i++)
+        column_size *= parents_num_values[i];
+
     for(int i=0; i<d.size(); i++)
     {
         int index = 0;
         int old = 1;
+        bool mark = false;
         for(int j=parents_num_values.size()-1; j>=0; j--)
         {
-            // Todo what if value_of_parj = ?
             string value_of_parj = d[i][parents_index[j]];
             index += parents[j]->get_value_index(value_of_parj)*old;
             old *= parents_num_values[j];
-            
-            cout<<parents[j]->get_name()<<" : "<<parents_num_values[j]<<" : "<<parents[j]->get_value_index(value_of_parj)<<endl;
         }
 
         string my_value = d[i][node.get_index()];
         index += node.get_value_index(my_value)*old;
-        cout<<node.get_name()<<" : "<<node.get_index()<<" : "<<node.get_value_index(my_value)<<endl;
-
         new_cpt[index] ++;
     }
-    int total = d.size();
-    for(int i=0; i<new_cpt.size(); i++)
-        new_cpt[i] = (new_cpt[i] + c)/(c*total);
-
+    
+    vector<int> sum_counts(column_size, 0);
+    for (int i=0; i<sum_counts.size(); i++)
+    {
+        int j=i;
+        while(j<new_cpt.size())
+        {
+            sum_counts[i] += new_cpt[j];
+            j += column_size;
+        }
+    }
+    
+    for (int i=0; i<sum_counts.size(); i++)
+    {
+        int j=i;
+        while(j<new_cpt.size())
+        {
+            new_cpt[j] = (new_cpt[j] + c)/(sum_counts[i] + c*node.get_num_values());
+            j += column_size;
+        }
+    }
     node.set_cpt(new_cpt);
-
 }
-// void maximization(Network &alarm, Data &data)
-// {
 
-// }
-int main()
+void update_network(Network &alarm, Data &data)
 {
-    Network alarm = read_network();
+    vector<Node>& nodes = alarm.get_nodes();
+
+    for(int i=0; i<nodes.size(); i++)
+        update_cpt(nodes[i], data, 0.001);
+}
+
+void em(Network &alarm, Data &data, int max_iter)
+{
+    for(int i=0;i<max_iter;i++)
+    {
+        printf("Step: %d\n", i);
+        get_new_data3(data, alarm);
+        update_network(alarm, data);
+    }
+}
+
+void init(Network &alarm, Data &data)
+{
+    vector<Node>& nodes = alarm.get_nodes();
+    int n = data.get_data().size();
+    for(int i=0; i<nodes.size(); i++)
+    {
+        nodes[i].init_cpt(0.001);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    Network alarm = read_network("alarm.bif");
     Data data("records.dat");
-    Node* test = (alarm.search_node("\"LVEDVolume\""));
-    update_cpt(*test, data, 1.0);
-    test->print_cpt();
+    init(alarm, data);
+    cout<<"Running Expectation Minimization\n";
+    em(alarm, data, 500);
+    write_network(alarm);
 }
